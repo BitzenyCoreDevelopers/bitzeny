9cef41051fe9449a8883f79b951f2c4b1bd6fbf5
Masao Hidemitsu <shirakox@protonmail.ch>
2017-12-27 20:28:48 +0900

    BitZeny: Change currency symbol

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case BTC: return QString("ZNY");
+    case mBTC: return QString("mZNY");
+    case uBTC: return QString::fromUtf8("μZNY");
     default: return QString("???");
     }
 }
