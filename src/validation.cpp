5a863e1d09a609ba9eec01e2d0380e1610e1a4df
Masao Hidemitsu <shirakox@protonmail.ch>
2018-01-13 13:06:19 +0900

    BitZeny3: Fix magic prefix for message signing

--- src/validation.cpp
+++ src/validation.cpp
@@ -233,7 +233,7 @@ CTxMemPool mempool(&feeEstimator);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const std::string strMessageMagic = "Bitcoin Signed Message:\n";
+const std::string strMessageMagic = "BitZeny Signed Message:\n";
 
 // Internal stuff
 namespace {
