389008e973551adf68a91c365838d5ed9a5b2b56
Masao Hidemitsu <shirakox@protonmail.ch>
2018-01-13 13:06:19 +0900

    BitZeny: Fix magic prefix for message signing

--- src/validation.cpp
+++ src/validation.cpp
@@ -94,7 +94,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const std::string strMessageMagic = "Bitcoin Signed Message:\n";
+const std::string strMessageMagic = "BitZeny Signed Message:\n";
 
 // Internal stuff
 namespace {
