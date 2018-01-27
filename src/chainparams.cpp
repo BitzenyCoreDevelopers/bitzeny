251a8488f4ecdbcb1a0111d8bbd4bab37cb52c52
Masao Hidemitsu <shirakox@protonmail.ch>
2018-01-27 21:40:56 +0900

    BitZeny: [testnet] Define start and end time for CSV and SegWit
    CSV
    - BIP68 - Relative lock-time using consensus-enforced sequence numbers
    - BIP112 - CHECKSEQUENCEVERIFY
    - BIP113 - Median time-past as endpoint for lock-time calculations
    
    SwgWit
    - BIP141 - Segregated Witness (Consensus layer)
    - BIP143 - Transaction Signature Verification for Version 0 Witness Program
    - BIP147 - Dealing with dummy stack element malleability

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -189,17 +189,15 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
 
-        // TODO
         // Deployment of BIP68, BIP112, and BIP113.
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1577836800; // January 1, 2020
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1609459200; // January 1, 2021
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1517097600; // January 28, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1548633600; // January 28, 2019
 
-        // TODO
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1577836800; // January 1, 2020
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1609459200; // January 1, 2021
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1517097600; // January 28, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1548633600; // January 28, 2019
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x0");
